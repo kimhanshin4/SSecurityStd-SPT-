5ebbfe81a50b357e814104aceadb8d0caaa925b2
kimhanshin4 <funold1004@gmail.com>
2023-12-21 15:22:26 +0900

    회원가입 로그인 숙제!

--- src/main/java/com/study/springsecuritystudy/user/controller/UserController.java
+++ src/main/java/com/study/springsecuritystudy/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.study.springsecuritystudy.user.controller;
 
 import com.study.springsecuritystudy.security.*;
 import com.study.springsecuritystudy.user.dto.*;
-import com.study.springsecuritystudy.user.dto.response.*;
 import com.study.springsecuritystudy.user.entity.*;
 import com.study.springsecuritystudy.user.service.*;
 import lombok.extern.slf4j.*;
@@ -16,32 +15,32 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     final UserService userService;
+    final JwtUtil jwtUtil;
 
     public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> createJWT(@RequestBody UserRequestDto requestDto) {
-        return new ResponseEntity<>(userService.login(requestDto), HttpStatus.OK);
+    @PostMapping("/jwt")
+    public ResponseEntity<String> createJWT() {
+        String username = "Funold";
+        UserRoleEnum role = UserRoleEnum.USER;
+
+        return new ResponseEntity<>(jwtUtil.createToken(username, role), HttpStatus.OK);
     }
 
-    //Hanshin
-/*    @GetMapping("/users")
-    public ResponseEntity<String> signup(@RequestBody UserRequestDto requestDto){
+    //m
+    @GetMapping("/users")
+    public ResponseEntity<String> signup(@RequestBody UserRequestDto requestDto) {
         userService.signup(requestDto);
-        return new ResponseEntity<>("좋아, 새 일원 이로군!",HttpStatus.OK);
+        return new ResponseEntity<>("좋아, 새 일원 이로군!", HttpStatus.OK);
     }
-    @PostMapping("/users")
-    public ResponseEntity<String>login(@RequestBody UserRequestDto requestDto){
-        userService.login(requestDto);
-        return new ResponseEntity<>("아 통과!",HttpStatus.OK);
-    }*/
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserRequestDto requestDto) {
-        User user = userService.createUser(requestDto);
 
-        return new ResponseEntity<>(new CreateUserResponse(user), HttpStatus.OK);
+    @PostMapping("/users")
+    public ResponseEntity<String> login(@RequestBody UserRequestDto requestDto) {
+        userService.login(requestDto);
+        return new ResponseEntity<>("아 통과!", HttpStatus.OK);
     }
 
 }
